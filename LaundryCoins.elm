c2e9ec5d4091f489d5e75b233bd22f478de84ab7
Sridhar Ratnakumar <srid@srid.ca>
2015-05-07 00:05:44 -0700

    port datatypes from Haskell
    and it works!

--- LaundryCoins.elm
+++ LaundryCoins.elm
@@ -1,6 +1,21 @@
 import Graphics.Element exposing (..)
 
+type Coin
+    = Quarter 
+    | Loonie
+    
+type alias Cents
+    = Int
+      
+cents : Coin -> Cents
+cents coin =
+    case coin of
+      Quarter -> 25
+      Loonie -> 100
+        
+showCoinsNeeded : Int -> String                
+showCoinsNeeded n = "XXX"
 
 main : Element
 main =
-  show "Hello, World!"
+  showCoinsNeeded 4 |> show
