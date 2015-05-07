983db9b23b2e8625c36709caef4912384c0cbd3c
Sridhar Ratnakumar <srid@srid.ca>
2015-05-07 00:59:49 -0700

    Accept input from the user
    this is cool

--- LaundryCoins.elm
+++ LaundryCoins.elm
@@ -1,6 +1,12 @@
 import Graphics.Element exposing (..)
+import Graphics.Input.Field as Field
 import List exposing (concat, repeat, append, foldr, map, filter, length) 
-import Html exposing (Html, div, text)
+import Html exposing (Html, fromElement, div, strong, text)
+import String
+import Maybe
+import Result
+
+-- Model
 
 type Coin
     = Quarter 
@@ -30,20 +36,39 @@ loonies = filter ((==) Loonie) >> length
 quarters : List Coin -> Int
 quarters = filter ((==) Quarter) >> length          
 
-showCoinsNeeded : Int -> Html                
-showCoinsNeeded n = let cost = nDaysCost n
-                        l = loonies cost
-                        q = quarters cost
-                        t = (sumCost cost) // 100 -- OK to discard remainder as our cost rounds to dollars
-                    in
+
+-- Input
+
+total : Signal.Mailbox Field.Content
+total = 
+    Signal.mailbox Field.noContent
+
+-- View
+
+viewReport : Int -> Html                
+viewReport n = let cost = nDaysCost n
+                   l = loonies cost
+                   q = quarters cost
+                   t = (sumCost cost) // 100 -- OK to discard remainder as our cost rounds to dollars
+               in
                       div []
                            [ div [] [ text <| "To do laundry " ++ toString n ++ " times, ask the cashier for:" ]
                            , div [] [ text <| toString l ++ " loonies," ]
                            , div [] [ text <| toString q ++ " quarters, from" ]
-                           , div [] [ text <| "$" ++ toString t ++ " cash back" ]
+                           , div [] [ strong [] [ text <| "$" ++ toString t ++ " cash back"  ] ]
                            ]
       
+view : Field.Content -> Html                      
+view fieldContent =
+    div []
+        [ fromElement <| Field.field Field.defaultStyle (Signal.message total.address) "4" fieldContent
+        , viewReport <| parseInt 4 fieldContent.string
+        ]
 
-main : Html
+parseInt : Int -> String -> Int
+parseInt default =
+   String.toInt >> Result.toMaybe >> Maybe.withDefault default 
+
+main : Signal Html
 main =
-  showCoinsNeeded 4
+  Signal.map view total.signal 
